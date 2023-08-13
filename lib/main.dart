f9538b7cb6effe342ab0f53d477ed92986bfcd70
Rana <ra01210772310@gmail.com>
2023-08-14 02:02:53 +0300

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nursery/cubit/bottom_nav_bar_cubit.dart';
+import 'package:nursery/features/onboarding_screen/presentation/views/activities_View/activities_components/bottle_view.dart';
+import 'package:nursery/features/onboarding_screen/presentation/views/activities_View/activities_components/classes_view.dart';
+import 'package:nursery/features/onboarding_screen/presentation/views/activities_View/activities_components/diaper_view.dart';
+import 'package:nursery/features/onboarding_screen/presentation/views/activities_View/activities_components/food_view.dart';
+import 'package:nursery/features/onboarding_screen/presentation/views/activities_View/activities_components/homework_view.dart';
+import 'package:nursery/features/onboarding_screen/presentation/views/activities_View/activities_components/medical_view.dart';
+import 'package:nursery/features/onboarding_screen/presentation/views/activities_View/activities_components/mood_view.dart';
+import 'package:nursery/features/onboarding_screen/presentation/views/activities_View/activities_components/nap_view.dart';
+import 'package:nursery/features/onboarding_screen/presentation/views/activities_View/activities_components/potty_view.dart';
+import 'package:nursery/features/onboarding_screen/presentation/views/activities_View/activities_components/quiz_view.dart';
 import 'package:nursery/views/screens/SplashScreen/SplashScreen.dart';
 import 'package:sizer/sizer.dart';
 void main() {
@@ -22,6 +32,18 @@ class MyApp extends StatelessWidget {
             child: MaterialApp(
               debugShowCheckedModeBanner: false,
               home: SplashScreen(),
+              routes: {
+                FoodView.routeName :(ctx)=> const FoodView(),
+                MedicalView.routeName :(ctx)=> const MedicalView(),
+                BottleView.routeName :(ctx)=> const BottleView(),
+                PottyView.routeName :(ctx)=>const PottyView(),
+                HomeWorkView.routeName :(ctx)=>const HomeWorkView(),
+                ClassesView.routeName :(ctx)=>const ClassesView(),
+                NapView.routeName :(ctx)=>const NapView(),
+                MoodView.routeName :(ctx)=>const MoodView(),
+                DiaperView.routeName :(ctx)=>const DiaperView(),
+                QuizView.routeName :(ctx)=>const QuizView(),
+              },
             ),
           );
         }
